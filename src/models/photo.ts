18f40c879ab0a6b6b1682aedd7470be55890fb56
adjong87 <adjong87@gmail.com>
2023-08-18 20:28:11 +0200

    added basic structure

---
+++ src/models/photo.ts
@@ -0,0 +1,9 @@
+export default interface IPhoto {
+    id?: string,
+    alt?: string,
+    link: string,
+    gemaaktDoor: string,
+    gemaaktMet: string,
+    createdAt?: string,
+    createdBy: string,
+}
