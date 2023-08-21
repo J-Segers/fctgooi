94375d168e36e53862061bb40c52ec2f407e9b09
J-Segers <jethroseger@gmail.com>
2023-08-21 16:26:02 +0200

    header sticky gemaakt en gewisseld met hero op homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import Header from '../components/header/Header';
 import Hero from '../components/hero/Hero';
+import "./Home.css";
 
 function Home() {
 
     return (
         <div id={"home-container"}>
             <Hero />
+            <Header />
             <section className="recente-activiteiten">
                 <article>
                     {/* hier kunnen in meerdere article elements de recente activiteiten geplaatst worden of in een nieuw component. */}
+                    &nbsp;
                 </article>
             </section>
         </div>
