08e318f58b807661a7995e173eca1db1bb82d0e4
J-Segers <jethroseger@gmail.com>
2023-08-22 02:08:01 +0200

    info bubbel toegevoegd met transitie aan de hero

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./Hero.css";
+import info from "../../assets/Information-Icon.png";
 import PropTypes from 'prop-types';
 
 Hero.propTypes = {
@@ -7,9 +8,31 @@ Hero.propTypes = {
 };
 
 function Hero() {
+    const maandFoto = {
+        fotograaf: "Pieter Post",
+        evenement: "club wedstrijd",
+        opmerking: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    }
     return (
         <div id="hero">
-            &nbsp;
+            <div className="maand-info">
+                <img src={info} alt="" />
+                <div className="info-title">
+                    <h2>Foto van de Maand</h2>
+                </div>
+                <div className="info-specs">
+                    <ul>
+                        <li>
+                            {`Gemaakt door: ${maandFoto?.fotograaf}`}
+                        </li>
+                        <li>
+                            {`Evenement: ${maandFoto?.evenement}`}
+                        </li>
+                        <li>{`${maandFoto?.opmerking}`}</li>
+                    </ul>
+
+                </div>
+            </div>
         </div>
     );
 }
