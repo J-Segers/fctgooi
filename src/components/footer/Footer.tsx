32608b2feda08e0a1777f7ab14e590ecceddc89e
J-Segers <jethroseger@gmail.com>
2023-09-02 14:43:15 +0200

    social media links toegepast in de footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -18,8 +18,12 @@ function Footer() {
             </section>
             <section className="social">
                 <h3>Volg ons op Social Media</h3>
-                <img className="instagram" src={instagram} alt="" />
-                <img className="facebook" src={facebook} alt="" />
+                <a href="https://www.instagram.com/fotoclubtgooi/" target='_blank'>
+                    <img className="instagram" src={instagram} alt="" />
+                </a>
+                <a href="https://www.facebook.com/www.fctgooi.nl/?locale=nl_NL" target='_blank'>
+                    <img className="facebook" src={facebook} alt="" />
+                </a>
             </section>
             <section className="meet">
                 Volgende bijeenkomst:<br />
