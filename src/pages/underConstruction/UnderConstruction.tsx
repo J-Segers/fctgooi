6c7d5299e42e2ed0f8ff4d12849cac66c97a3a84
J-Segers <jethroseger@gmail.com>
2023-09-18 00:18:17 +0200

    underconstruction component aangepast en gestyled.

--- src/pages/underConstruction/UnderConstruction.tsx
+++ src/pages/underConstruction/UnderConstruction.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
+import "./UnderConstruction.css";
 import uc from '../../assets/underConstruction.png';
 import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
+import { Link } from 'react-router-dom';
 
 function UnderConstruction() {
     return (
         <>
             <Header />
-            <img src={uc} alt="" />
+            <div className="construction-container">
+                <section className="contact-info">
+                    <h1>
+                        Website is<br/>
+                        <span>under construction</span>
+                    </h1>
+                    <article>
+                        Er wordt gewerkt aan de website.<br />
+                        Voor actuele informatie kunt u contact opnemen via de contact pagina.
+                    </article>
+                    <Link className="contact-button" to={"/contact"}>
+                        CONTACT
+                    </Link>
+                </section>
+                <img src={uc} alt="under construction" />
+            </div>
             <Footer />
             
         </>
