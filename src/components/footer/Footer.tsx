65c489fc2bb0c74408c1cb3bea24c4acf4bd3b2e
J-Segers <jethroseger@gmail.com>
2023-09-19 00:41:39 +0200

    rel="noopener noreferrer" toegevoegd aan externe links in de footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,17 +2,9 @@ import React, { useEffect } from 'react';
 import "./Footer.css";
 import instagram from "../../assets/instagram-icon.svg";
 import facebook from "../../assets/facebook-icon.svg";
-import PropTypes from 'prop-types';
 import { getNextMeeting } from '../../helperFunctions/nextmeet';
 
-Footer.propTypes = {
-    
-};
-
 function Footer() {
-
-    let meeting: Date = new Date();
-
     return (
         <footer>
             <section className="adres">
@@ -22,10 +14,10 @@ function Footer() {
             </section>
             <section className="social">
                 <h3>Volg ons op Social Media</h3>
-                <a href="https://www.instagram.com/fotoclubtgooi/" target='_blank'>
+                <a href="https://www.instagram.com/fotoclubtgooi/" target='_blank' rel="noopener noreferrer">
                     <img className="instagram" src={instagram} alt="" />
                 </a>
-                <a href="https://www.facebook.com/www.fctgooi.nl/?locale=nl_NL" target='_blank'>
+                <a href="https://www.facebook.com/www.fctgooi.nl/?locale=nl_NL" target='_blank' rel="noopener noreferrer">
                     <img className="facebook" src={facebook} alt="" />
                 </a>
             </section>
