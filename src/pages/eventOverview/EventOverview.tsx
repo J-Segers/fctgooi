bca5656905f681541f572b6a8f60a1c630b0f747
adjong87 <adjong87@gmail.com>
2023-09-24 21:07:42 +0200

    made Clubtochten page generic to use for all 4 event types, made Enum with EventSoorten and EventInfo constant

--- src/pages/eventOverview/EventOverview.tsx
+++ src/pages/eventOverview/EventOverview.tsx
@@ -2,26 +2,49 @@ import React, {useEffect, useState} from 'react';
 import {InferProps} from 'prop-types';
 import Header from "../../components/header/Header";
 import Spacer from "../../components/spacer/Spacer";
-import dataArr from "../../data/homeData";
 import Post from "../../components/preview/Preview";
 import Footer from "../../components/footer/Footer";
 import IEvent from "../../models/eventItem";
 import EventService from "../../services/EventService";
 import {sortEventsDesc} from "../../utils/helpers/sorters";
+import {EventInfo, EventSoort} from "../../utils/CONSTANTS";
 
-ClubTochten.propTypes = {};
+EventOverview.propTypes = {
+    type: EventSoort,
+};
 
-function ClubTochten({}: InferProps<typeof ClubTochten.propTypes>) {
-    const [clubtochten, setClubtochten] = useState<Array<IEvent>>([])
-    const sorted: IEvent[] = sortEventsDesc(clubtochten);
+function EventOverview({type}: InferProps<typeof EventOverview.propTypes>) {
+    const [events, setEvents] = useState<Array<IEvent>>([])
+    const sorted: IEvent[] = sortEventsDesc(events);
+    const [pageInfo, setPageInfo] = useState({
+        beschrijving: [""],
+        titel: ""
+    })
 
     useEffect(() => {
-        EventService.getByQuery("Clubtocht")
-            .then(res => {
-                setClubtochten(res)
-            })
+        EventService.getByQuery(type)
+            .then(res => setEvents(res))
             .catch(e => console.error(e))
+            .finally(() => getPageInfo(type))
     }, []);
+
+    function getPageInfo(type: EventSoort): void {
+        console.log(type)
+        switch(type) {
+            case EventSoort.Clubtocht:
+                setPageInfo(EventInfo.clubtochten)
+                break;
+            case EventSoort.Expositie:
+                setPageInfo(EventInfo.exposities)
+                break;
+            case EventSoort.DeBesteClub:
+                setPageInfo(EventInfo.debesteclub)
+                break;
+            case EventSoort.Bondsfotowedstrijd:
+                setPageInfo(EventInfo.bondsfotowedstrijd)
+                break;
+        }
+    }
 
 
     let count: number = 0;
@@ -36,24 +59,15 @@ function ClubTochten({}: InferProps<typeof ClubTochten.propTypes>) {
     }
 
     return (
-        <div id={"clubtochten-container"}>
+        <div id={"eventOverview-container"}>
             <Header/>
             <main>
-                <section id="clubtochten-info">
-                    <h1>Clubtochten</h1>
-                    <p>
-                        Tenminste één keer per jaar trekken we er met zijn allen op uit<br/>
-                        Samen gaan we naar een eerder gekozen bestemming om daar samen foto's te maken<br/>
-                        <br/>
-                        Naast de fotografische inhoud van de tocht heeft het onderlinge, ongedwongen contact ook een
-                        sociaal aspect.<br/>
-                        <br/>
-                        En zeg nou zelf, even lekker eropuit is toch heerlijk<br/>
-                        Friesland, Groningen, België, we komen overal! <br/>
-                    </p>
+                <section id="event-info">
+                    <h1>{pageInfo.titel}</h1>
+                    {pageInfo.beschrijving.map((line => <p>{line}</p>))}
                 </section>
                 <Spacer/>
-                <section className="recente-activiteiten">
+                <section className="overview-container">
                     {sorted.map((event: IEvent) => {
 
                         let currentOrientation:string = orientation;
@@ -92,4 +106,4 @@ function ClubTochten({}: InferProps<typeof ClubTochten.propTypes>) {
     );
 }
 
-export default ClubTochten;
+export default EventOverview;
